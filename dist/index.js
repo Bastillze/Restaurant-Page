a932dfadd7012e7a8934e73973c6deae77bbc9ff
Lance <contact@lefside.com>
2023-01-11 20:29:06 -0500

    Tenth commit finalizing dist and src modules

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,27 @@
-import * as module from  './initial_page_load.js';
-module.initialPageLoad();
+import {initialPageLoad} from './initial_page_load.js';
 
+import {menu} from "./src/Menu_Tab/MenuTab.js";
+
+import {contact} from "./src/Contact_Tab/ContactTab.js";
+
+initialPageLoad();
+
+
+/* Tab-Switching module */
+
+let TabSwitchingModule = {function() {
+
+    const homeTab = document.querySelector(".home");
+    homeTab.addEventListener("click", inititalPageLoad);
+    
+    const menuTab = document.querySelector(".menu");
+    menuTab.addEventListener("click" , menu);
+    
+    
+    const contactTab = document.querySelector(".contact");
+    contactTab.addEventListener("click" , contact);
+    
+    
+ }
+       
+}
